af83dc574b61bd434139508d83397940db47952e
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2025-11-06 14:15:17 +0800

    Make changes in MovieCard and its .css files to make entire movie button clickable

--- client/src/components/MovieCard.js
+++ client/src/components/MovieCard.js
@@ -6,20 +6,18 @@ const MovieCard = ({ movie }) => {
   if(!movie) return null
 
   return (
-    <div className='movie-card'>
-      <Link to={{ pathname: '/tickets',}} state={{ movieId: movie.id, movie: movie }}>      
-        <div className='movie-info'>
-          <div className='movie-detail'>
-            <span className='label'>Title:</span>
-            <span className='value'>{movie.title}</span>
-          </div>
-          <div className='movie-detail'>
-            <span className='label'>Genre:</span>
-            <span className='value'>{movie.genre}</span>
-          </div>
-        </div>      
-      </Link>
-    </div>
+    <Link to={{ pathname: '/tickets',}} state={{ movieId: movie.id, movie: movie }} className='movie-card'>
+      <div className='movie-info'>
+        <div className='movie-detail'>
+          <span className='label'>Title:</span>
+          <span className='value'>{movie.title}</span>
+        </div>
+        <div className='movie-detail'>
+          <span className='label'>Genre:</span>
+          <span className='value'>{movie.genre}</span>
+        </div>
+      </div>      
+    </Link>
   )
 }
 
